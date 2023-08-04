008a4504594f116e71868a27fb925703f7145dd3
epedrotti7 <pedrottieliton777@gmail.com>
2023-08-04 20:41:23 -0300

    first commit

--- src/views/PlayPage.vue
+++ src/views/PlayPage.vue
@@ -28,7 +28,8 @@
 
             <ion-col size="12" class="ion-text-center select-style-2" v-if="!isLoading && !isQuestionGenerated">
               <ion-select placeholder="Selecione a Dificuldade" v-model="selectedDifficulty">
-                <ion-select-option v-for="difficulty in difficultyOptions" :value="difficulty" :key="difficulty">{{ difficulty }}</ion-select-option>
+                <ion-select-option v-for="difficulty in difficultyOptions" :value="difficulty" :key="difficulty">{{
+                  difficulty }}</ion-select-option>
               </ion-select>
             </ion-col>
 
@@ -44,21 +45,24 @@
               <Image imagem="../public/codeshow.png" />
             </ion-col>
 
-
-
-            <ion-col size="12" class="ion-text-center text-style-5" v-if="!isLoading && isQuestionGenerated && questionData">
+            <ion-col size="12" class="ion-text-center text-style-5"
+              v-if="!isLoading && isQuestionGenerated && questionData && !answerMessage && !correctCode">
               <Text>{{ questionData.question }}</Text>
             </ion-col>
 
-
-            <ion-col size="12" class="ion-text-center" v-if="!isLoading && isQuestionGenerated && questionData">
+            <ion-col size="12" class="ion-text-center" v-if="!isLoading && isQuestionGenerated && questionData && !answerMessage && !correctCode">
               <div v-for="(option, index) in questionData.alternatives" :key="index" class="question-option">
-                <input type="radio" :id="'option'+index" :value="option" v-model="selectedAnswer" class="question-input">
-                <label :for="'option'+index" class="question-label">{{ option }}</label>
+                <input type="radio" :id="'option' + index" :value="option" v-model="selectedAnswer" class="question-input">
+                <label :for="'option' + index" class="question-label">{{ option }}</label>
               </div>
               <CustomButton class="button-style" @click="submitAnswer" :icon="playSharp" texto="Submeter" />
             </ion-col>
-            
+
+            <ion-col size="12" class="ion-text-center" v-if="!isLoading && isQuestionGenerated">
+              <IonIcon :icon="correctCode === 1 ? checkmarkOutline : closeOutline" class="response-icon" />
+              <Text class="response-message">{{ answerMessage }}</Text>
+            </ion-col>
+
           </ion-row>
         </ion-grid>
       </div>
@@ -69,7 +73,7 @@
 <script setup lang="ts">
 import { IonPage, IonContent, IonGrid, IonRow, IonCol, IonSelect, IonSelectOption, IonItem, IonIcon, IonList, IonRadio, IonLabel } from '@ionic/vue';
 import { ref, onMounted } from 'vue';
-import { playSharp } from 'ionicons/icons';
+import { playSharp, checkmarkOutline, closeOutline } from 'ionicons/icons';
 import axios from 'axios';
 
 import Image from '@/components/Image.vue';
@@ -84,8 +88,9 @@ const difficultyOptions = ['Fácil', 'Médio', 'Difícil'];
 let selectedTech = ref('');
 let selectedDifficulty = ref('');
 let isLoading = ref(false);
-
 let isQuestionGenerated = ref(false);
+let answerMessage = ref('');  // nova ref para armazenar a mensagem da resposta
+let correctCode = ref(0);
 
 let questionData = ref({
   id: '',
@@ -105,40 +110,71 @@ onMounted(() => {
   StatusBar.setOverlaysWebView({ overlay: true });
 });
 
-defineExpose({
-  selectedTech,
-  selectedDifficulty,
-  user
-});
-
-async function startLoading() { // Adicionamos async para fazer chamadas assíncronas
+async function startLoading() {
   isLoading.value = true;
 
-  // Aqui, colocamos o código da chamada à API
+
   try {
     const response = await axios.post('http://localhost:3000/api/v1/question', {
       nivel: selectedDifficulty.value,
       tecnologia: selectedTech.value
-    },{
+    }, {
       headers: {
         Authorization: `Bearer ${user.authToken}`
       }
     });
 
-    questionData.value = response.data; // Exibindo a resposta no console
-    isQuestionGenerated.value = true; // Quando a pergunta é gerada, setamos isQuestionGenerated como true
+    questionData.value = response.data;
+    isQuestionGenerated.value = true;
 
   } catch (error) {
-    console.error(error); // Tratamento de erro
+    console.error(error);
   } finally {
-    isLoading.value = false; // Garantindo que isLoading será definido como false mesmo se houver um erro
+    isLoading.value = false;
   }
 }
 
-function submitAnswer() {
-  // Aqui você pode tratar o envio da resposta selecionada
-  console.log('Resposta selecionada:', selectedAnswer.value);
+async function submitAnswer() {
+  isLoading.value = true;
+
+  let answerValueFormatted = String(selectedAnswer.value).split(")")[0];
+
+  try {
+    const response = await axios.post(`http://localhost:3000/api/v1/answer/${questionData.value.id}`, {
+      answer: answerValueFormatted,
+    }, {
+      headers: {
+        Authorization: `Bearer ${user.authToken}`
+      }
+    });
+
+    isQuestionGenerated.value = true;
+    answerMessage.value = response.data.message;
+    correctCode.value = response.data.correctCode
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isLoading.value = false;
+  }
 }
+
+defineExpose({
+  selectedTech,
+  selectedDifficulty,
+  user,
+  playSharp,
+  isLoading,
+  isQuestionGenerated,
+  startLoading,
+  submitAnswer,
+  techOptions,
+  difficultyOptions,
+  questionData,
+  selectedAnswer,
+  answerMessage,
+  correctCode
+});
 </script>
 
 <style scoped>
@@ -150,6 +186,9 @@ function submitAnswer() {
   overflow-y: auto;
   overflow-x: hidden;
   box-sizing: border-box;
+}
+
+.response-message{
 }
 
 .text-style-1 {
@@ -211,14 +250,14 @@ function submitAnswer() {
 }
 
 .question-option {
-  font-size: 24px; 
-  margin-bottom: 20px; 
+  font-size: 24px;
+  margin-bottom: 20px;
   margin-top: 30px;
 }
 
-.question-input, .question-label {
-  margin-left: 10px; 
-
+.question-input,
+.question-label {
+  margin-left: 10px;
   padding: 10px;
 }
 
@@ -230,6 +269,12 @@ function submitAnswer() {
   width: 70%;
   margin: 0 auto;
   border-bottom: 1px solid #16CE92;
+}
+
+.response-icon{
+  color: #16CE92;
+  margin-top: 180px;
+  font-size: 50px;
 }
 
 .input-icon {
@@ -279,5 +324,4 @@ function submitAnswer() {
   font-size: 20px;
   margin-left: 80px;
   text-align: center;
-}
-</style>
+}</style>
